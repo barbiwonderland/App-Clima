5773bd1daecc440171d6c381bb06f29278eed66e
hotelufo <baarbarabottazzi@gmail.com>
2020-09-26 17:36:34 -0300

    fondoytransp

--- button.js
+++ button.js
@@ -26,14 +26,25 @@ function boton(){
 
 
        `;
-       if (data.weather[0].description.includes("lluvia")) {document.body.style.background= "url('https://img.freepik.com/free-photo/cute-couple-hugging-rain_13339-248233.jpg?size=626&ext=jpg')"; document.body.style.backgroundSize="cover";
-      }  else if (data.main.temp<10) {document.body.style.background= "url('https://ng.kcsd.org/wp-content/uploads/sites/7/2020/01/snow-720x477.jpg')"; document.body.style.backgroundSize="cover";
-     
- } else if(data.main.temp<17){
-    document.body.style.background="url('https://reesephoto.files.wordpress.com/2012/10/fall-tree-color2.jpg')"; document.body.style.backgroundSize="cover";
+       let imagen =document.body;
+       if (data.weather[0].description.includes("chubasco" ||"lluvia") ){
+        
+            imagen.style.backgroundImage= "url('imagenes/lluvia.jpg')"; document.body.style.backgroundSize="cover"; 
+       }else if (data.main.temp<10) {
+        imagen.style.backgroundImage= "url('imagenes/frio.jpg')"; 
+
+           
+       } else if(data.main.temp<23){
+
+      imagen.style.backgroundImage="url('imagenes/otoño.jpg')"; document.body.style.backgroundSize="cover"; 
+  
+
      
  }else{
-    document.body.style.background="url('https://uknow.uky.edu/sites/default/files/styles/facebook/public/GettyImages-1160947136%20%281%29.jpg?itok=16WaT0Uy')"; document.body.style.backgroundSize="cover";
+   
+        document.body.style.backgroundImage="url('imagenes/calor.jpg')"; document.body.style.backgroundSize="cover";
+
+   
      
 }
     })};
