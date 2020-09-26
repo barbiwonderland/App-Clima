a6e4076c90b4dedf31a25aa637e74ef6ef00cc18
hotelufo <baarbarabottazzi@gmail.com>
2020-09-26 00:33:41 -0300

    cambiobackground

--- button.js
+++ button.js
@@ -26,7 +26,16 @@ function boton(){
 
 
        `;
-    
-  
+ if (data.main.temp<10) {document.body.style.background= "url('https://ng.kcsd.org/wp-content/uploads/sites/7/2020/01/snow-720x477.jpg')"; document.body.style.backgroundSize="cover";
+     
+ }
+ else if(data.main.temp<17){
+    document.body.style.background="url('https://reesephoto.files.wordpress.com/2012/10/fall-tree-color2.jpg')"; document.body.style.backgroundSize="cover";
+     
+ }
+else{
+    document.body.style.background="url('https://uknow.uky.edu/sites/default/files/styles/facebook/public/GettyImages-1160947136%20%281%29.jpg?itok=16WaT0Uy')"; document.body.style.backgroundSize="cover";
+     
+}
     })};
 boton();
